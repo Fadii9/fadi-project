e90c0e3136bfa349e5fbdb912ee0045deb03dfd6
Fadi <fadi_makhoul1@hotmail.com>
2022-01-17 14:42:13 +0200

    added data files

---
+++ src/data/meals.ts
@@ -0,0 +1,24 @@
+type mealsList = {
+    mealName: string;
+    ingredients: string[]
+}[]
+
+const meals: mealsList = [{
+    mealName: "Hamburger",
+    ingredients: ["Bread" , "Patty" , "Tomato", "pickles", "Lettuce", "onions"]
+}, {
+    mealName: "Salad",
+    ingredients: ["Tomato", "Cucumber", "Lettuce" , "onions" ]
+}, {
+    mealName: "Steak",
+    ingredients: ["meat"]
+}, {
+    mealName: "Pizza",
+    ingredients: ["Dough" , "Tomato-sauce", "cheese"]
+}, {
+    mealName: "Spaghetti",
+    ingredients: ["Spaghetti", "Tomato-sauce"]
+}, {
+    mealName: "Baguette",
+    ingredients: ["Dough"]
+}]
---
+++ src/data/queues.ts
@@ -0,0 +1,27 @@
+import customers ,{customersList} from "./customers"
+
+let waitingCustomers:customersList = customers;
+
+let queue1:customersList = [];
+let queue2:customersList = [];
+let queue3:customersList = [];
+
+if (queue1.length == 0 && queue2.length == 0 && queue3.length == 0){
+    if (waitingCustomers.length >= 15){
+        queue1= [waitingCustomers.shift()! , ...queue1]
+        queue2= [waitingCustomers.shift()! , ...queue2]
+        queue3= [waitingCustomers.shift()! , ...queue3]
+    }
+}
+
+let allQueues: customersList[] = [queue1, queue2, queue3]
+
+let shortest = allQueues.reduce(function(p,c) {return p.length>c.length?c:p;});
+let longest = allQueues.reduce(function(p,c) {return p.length<c.length?c:p;});
+
+
+console.log(queue1)
+console.log(queue2)
+console.log(queue3)
+console.log(waitingCustomers)
+
