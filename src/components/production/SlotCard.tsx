b2a1ab77a6001509c5ae43a75bdcc8036eea0db4
Fadi <fadi_makhoul1@hotmail.com>
2022-01-30 15:26:33 +0200

    updated - handling multiple meals in orders

--- src/components/production/SlotCard.tsx
+++ src/components/production/SlotCard.tsx
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "../../store/index";
+import React, {useState, useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {RootState} from "../../store/index";
 
 import "./SlotCard.css";
 
 import mealsData from "../../data/mealsData";
 import ingsData from "../../data/ingredientsData";
 
-import { slotsActions } from "../../store/slots";
-import { queuesActions } from "../../store/queues";
-import { deliveriesActions } from "../../store/deliveries";
+import {slotsActions} from "../../store/slots";
+import {queuesActions} from "../../store/queues";
+import {deliveriesActions} from "../../store/deliveries";
 
 type SlotNumber = "1" | "2" | "3";
 type SlotState = `slot${SlotNumber}State`;
@@ -18,156 +18,154 @@ type DeliveryNumber = 1 | 2 | 3;
 type DeliveryState = `addToDelivery${DeliveryNumber}`;
 
 const SlotCard: React.FC<{
-  inUse: boolean;
-  time: number;
-  slotNumber: SlotNumber;
-}> = ({ inUse, time, slotNumber }) => {
-  const dispatch = useDispatch();
-  let image;
-  let queue = [];
-  let showIngs;
-  let estTime = 0;
-  let receivedTime = 0;
-  let producing = false;
-  const [startTime, setStartTime] = useState(0);
-  const slotName: SlotState = `slot${slotNumber}State`;
-  const slot = useSelector((state: RootState) => state.slotsSlice[slotName]);
-  let emptySlot = !slot.id;
+    inUse: boolean;
+    time: number;
+    slotNumber: SlotNumber;
+}> = ({inUse, time, slotNumber}) => {
+    const dispatch = useDispatch();
+    let image;
+    let queue = [];
+    let showIngs;
+    let mealsIngs: string[] = [];
+    let estTime = 0;
+    let meal;
+    let producing = false;
+    let preparedAllMeals = false;
+    const [startTime, setStartTime] = useState(0);
+    const slotName: SlotState = `slot${slotNumber}State`;
+    const slot = useSelector((state: RootState) => state.slotsSlice[slotName]);
+    let emptySlot = !slot.id;
 
-  //queues states
-  const queue1 = useSelector(
-    (state: RootState) => state.queuesSlice.queue1State
-  );
-  const queue2 = useSelector(
-    (state: RootState) => state.queuesSlice.queue2State
-  );
-  const queue3 = useSelector(
-    (state: RootState) => state.queuesSlice.queue3State
-  );
-  const allQueues = [queue1, queue2, queue3];
+    //queues states
+    const queue1 = useSelector(
+        (state: RootState) => state.queuesSlice.queue1State
+    );
+    const queue2 = useSelector(
+        (state: RootState) => state.queuesSlice.queue2State
+    );
+    const queue3 = useSelector(
+        (state: RootState) => state.queuesSlice.queue3State
+    );
+    const allQueues = [queue1, queue2, queue3];
 
-  //available delivery state
-  let deliveryNumber: DeliveryNumber = useSelector(
-    (state: RootState) => state.deliveriesSlice.availbleDeliveryState
-  );
+    //available delivery state
+    let deliveryNumber: DeliveryNumber = useSelector(
+        (state: RootState) => state.deliveriesSlice.availbleDeliveryState
+    );
 
-  //check if there is a customer in each queue
-  const [cusInQ1, cusInQ2, cusInQ3] = [
-    queue1.length > 0,
-    queue2.length > 0,
-    queue3.length > 0,
-  ];
+    //check if there is a customer in each queue
+    const [cusInQ1, cusInQ2, cusInQ3] = [
+        queue1.length > 0,
+        queue2.length > 0,
+        queue3.length > 0,
+    ];
 
-  // find the queue with the first customer that got in
-  if (cusInQ1 && !cusInQ2 && !cusInQ3) {
-    queue = queue1;
-  } else if (cusInQ1 && cusInQ2 && !cusInQ3) {
-    queue1[0].addedTime < queue2[0].addedTime
-      ? (queue = queue1)
-      : (queue = queue2);
-  } else if (cusInQ1 && cusInQ2 && cusInQ3) {
-    queue = [queue1, queue2, queue3].reduce(function (prev, curr) {
-      return prev[0].addedTime < curr[0].addedTime ? prev : curr;
-    });
-  }
-
-  // removing first customer in line, and adding to preperation slot
-  if (queue.length > 1 && emptySlot && time % 2 == 0) {
-    dispatch(slotsActions[`addToSlot${slotNumber}`](queue[0]));
-    localStorage.setItem(`slot${slotNumber}`, JSON.stringify(queue[0]));
-
-    switch (queue) {
-      case queue1:
-        dispatch(queuesActions.removeFromQueue1());
-
-        break;
-      case queue2:
-        dispatch(queuesActions.removeFromQueue2());
-
-        break;
-      case queue3:
-        dispatch(queuesActions.removeFromQueue3());
-
-        break;
+    // find the queue with the first customer that got in
+    if (cusInQ1 && !cusInQ2 && !cusInQ3) {
+        queue = queue1;
+    } else if (cusInQ1 && cusInQ2 && !cusInQ3) {
+        queue1[0].addedTime < queue2[0].addedTime
+            ? (queue = queue1)
+            : (queue = queue2);
+    } else if (cusInQ1 && cusInQ2 && cusInQ3) {
+        queue = [queue1, queue2, queue3].reduce(function (prev, curr) {
+            return prev[0].addedTime < curr[0].addedTime ? prev : curr;
+        });
     }
-  }
 
-  // set statring meal preperation time
-  useEffect(() => {
-    setStartTime(time);
-  }, [slot]);
+    // removing first customer in line, and adding to preparation slot
+    if (queue.length > 1 && emptySlot && time % 2 == 0) {
+        dispatch(slotsActions[`addToSlot${slotNumber}`](queue[0]));
+        localStorage.setItem(`slot${slotNumber}`, JSON.stringify(queue[0]));
 
-  // getting meals ingredients & detailes
-  if (!emptySlot) {
-    producing = true;
-    let meals = slot.order;
-    let mealsIngs = meals.map((meal: string) => {
-      for (let i in mealsData) {
-        if (mealsData[i].mealName == meal) return mealsData[i].ingredients;
-      }
-    });
+        switch (queue) {
+            case queue1:
+                dispatch(queuesActions.removeFromQueue1());
 
-    // find image according to the meal
-    image = meals.map((meal: string) => {
-      for (let i in mealsData) {
-        if (mealsData[i].mealName == meal) return mealsData[i];
-      }
-    });
-    showIngs = mealsIngs.join(",");
+                break;
+            case queue2:
+                dispatch(queuesActions.removeFromQueue2());
 
-    // calculating meal preperation time according to the ingredients
-    mealsIngs[0].map((ing: string) => {
-      estTime += ingsData.find((o) => o.ing === ing)!.prepTime;
-    });
-    estTime -= time - startTime;
-  }
+                break;
+            case queue3:
+                dispatch(queuesActions.removeFromQueue3());
 
-  // checking if finished preparing meal and send to available delivery
-  if (
-    !emptySlot &&
-    producing &&
-    estTime == 0
-  ) {
-    producing = false;
+                break;
+        }
+    }
 
-    let DeliveryAction: DeliveryState = `addToDelivery${deliveryNumber}`;
-    dispatch(deliveriesActions[DeliveryAction](slot));
-    localStorage.setItem(`delivery${deliveryNumber}`, JSON.stringify(slot));
+    // set statring meal preperation time
+    useEffect(() => {
+        setStartTime(time);
+    }, [slot]);
 
-    dispatch(slotsActions[`emptySlot${slotNumber}`]());
-    localStorage.setItem(`slot${slotNumber}`, JSON.stringify({}));
+    // getting meals ingredients & image
+    if (!emptySlot) {
+        producing = true;
+        let meals = slot.order;
+        if (meals.length > 0) {
+            meal = meals[0];
+            for (let i in mealsData) {
+                if (mealsData[i].mealName == meal) {
+                    mealsIngs = mealsData[i].ingredients;
+                    image = mealsData[i].imageUrl;
+                }
+            }
+            showIngs = mealsIngs.join(",");
 
-  }
+            // calculating meal preparation time according to the ingredients
+            mealsIngs.map((ing: string) => {
+                estTime += ingsData.find((o) => o.ing === ing)!.prepTime;
+            });
+            estTime -= time - startTime;
+        } else {
+            preparedAllMeals = true; // if finished preparing all meals ,set preparedAllMeals to true
+        }
 
-  return inUse ? (
-    <div className={"slot"}>
-      <div className={"slot_image_container"}>
-        {emptySlot ? (
-          "Empty Slot"
-        ) : (
-          <img
-            src={image[0].imageUrl}
-            alt="Preparing!"
-            className={"slot_image"}
-          />
-        )}
-      </div>
-      <div className={"slot_details"}>
-        <span className={"title"}>Order ID: </span>
-        {slot.id} <br />
-        <span className={"title"}>Producing: </span>
-        {slot.order} <br />
-        <span className={"title"}>Estimated Time: </span>{" "}
-        {estTime > 0 && `${estTime} Seconds`}
-      </div>
-      <div className={"slot_status"}>
-        <div className={"slot_status_text"}>Production Status:</div>
-        <div className={"slot_status_ings"}>{showIngs}</div>
-      </div>
-    </div>
-  ) : (
-    <div className={"slot"}>Not In Use</div>
-  );
+        // if finished a meal, remove it meals list, and start preparing the next meal
+        if (estTime === 0) {
+            meals = meals.slice(1)
+            dispatch(slotsActions[`addToSlot${slotNumber}`]({...slot, order: meals}));
+        }
+    }
+
+    // checking if finished preparing meal and send to available delivery
+    if (!emptySlot && producing && preparedAllMeals) {
+        producing = false;
+
+        let DeliveryAction: DeliveryState = `addToDelivery${deliveryNumber}`;
+        dispatch(deliveriesActions[DeliveryAction](slot));
+        localStorage.setItem(`delivery${deliveryNumber}`, JSON.stringify(slot));
+
+        dispatch(slotsActions[`emptySlot${slotNumber}`]());
+        localStorage.setItem(`slot${slotNumber}`, JSON.stringify({}));
+    }
+
+    return inUse ? (
+        <div className={"slot"}>
+            <div className={"slot_image_container"}>
+                {emptySlot ? (
+                    "Empty Slot"
+                ) : (
+                    <img src={image} alt="Preparing!" className={"slot_image"}/>
+                )}
+            </div>
+            <div className={"slot_details"}>
+                <span className={"title"}>Order ID: </span>
+                {slot.id} <br/>
+                <span className={"title"}>Producing: </span>
+                {meal} <br/>
+                <span className={"title"}>Estimated Time: </span>{" "}
+                {estTime > 0 && `${estTime} Seconds`}
+            </div>
+            <div className={"slot_status"}>
+                <div className={"slot_status_text"}>Production Status:</div>
+                <div className={"slot_status_ings"}>{showIngs}</div>
+            </div>
+        </div>
+    ) : (
+        <div className={"slot"}>Not In Use</div>
+    );
 };
 
 export default SlotCard;
--- src/pages/Restaurant/Restaurant.tsx
+++ src/pages/Restaurant/Restaurant.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { useTimer } from "use-timer";
-import { RootState } from "../../store/index";
+import React, {useEffect} from "react";
+import {useSelector, useDispatch} from "react-redux";
+import {useTimer} from "use-timer";
+import {RootState} from "../../store/index";
 
 import "./Restaurant.css";
 
@@ -11,96 +11,106 @@ import Customers from "../../components/customers/Customers";
 import Production from "../../components/production/Production";
 
 // states actions
-import { slotsActions } from "../../store/slots";
-import { queuesActions } from "../../store/queues";
-import { customersActions } from "../../store/customers";
-import { deliveriesActions } from "../../store/deliveries";
+import {slotsActions} from "../../store/slots";
+import {queuesActions} from "../../store/queues";
+import {customersActions} from "../../store/customers";
+import {deliveriesActions} from "../../store/deliveries";
 
 const Restaurant: React.FC = () => {
-  const dispatch = useDispatch();
-  let initialTime;
-  let timeInterval = 500;
-  let myStorage = window.localStorage;
+    const dispatch = useDispatch();
+    let initialTime;
+    let timeInterval = 500;
+    let myStorage = window.localStorage;
 
-  if (localStorage.time){
-    initialTime = JSON.parse(myStorage.time)
-  } else {
-    initialTime = 0;
-  }
-
-  // setting the timer
-    const { time, start, pause } = useTimer({
-    autostart: false,
-    initialTime: initialTime,
-    interval: timeInterval,
-  });
-
-  localStorage.setItem(`time`, JSON.stringify(time));
-
-
-  const waitingCustomers = useSelector(
-    (state: RootState) => state.customersSlice.customersState
-  );
-
-  //checks localstorage for each state, if there is a stored value, update state
-  useEffect(() => {
-    if (myStorage.waitingCustomers) {
-      dispatch(customersActions.localStorageToWating(JSON.parse(myStorage.waitingCustomers)));
-    }
-    if (myStorage.queue1 && myStorage.queue1 != "[]") {
-      dispatch(queuesActions.localStorageToQueue1(JSON.parse(myStorage.queue1)));
-    }
-    if (myStorage.queue2 && myStorage.queue2 != "[]") {
-      dispatch(queuesActions.localStorageToQueue2(JSON.parse(myStorage.queue2)));
-    }
-    if (myStorage.queue3 && myStorage.queue3 != "[]") {
-      dispatch(queuesActions.localStorageToQueue3(JSON.parse(myStorage.queue3)));
+    if (localStorage.time) {
+        initialTime = JSON.parse(myStorage.time)
+    } else {
+        initialTime = 0;
     }
 
-    if (myStorage.slot1 && myStorage.slot1 != "{}") {
-      dispatch(slotsActions.localStorageToSlot1(JSON.parse(myStorage.slot1)));
-    }
-    if (myStorage.slot2 && myStorage.slot2 != "{}") {
-      dispatch(slotsActions.localStorageToSlot2(JSON.parse(myStorage.slot2)));
-    }
-    if (myStorage.slot3 && myStorage.slot3 != "{}") {
-      dispatch(slotsActions.localStorageToSlot3(JSON.parse(myStorage.slot3)));
-    }
-    if (myStorage.delivery1 && myStorage.delivery1 != "{}") {
-      dispatch(deliveriesActions.localStorageToDelivery1(JSON.parse(myStorage.delivery1)));
-    }
-    if (myStorage.delivery2 && myStorage.delivery2 != "{}") {
-      dispatch(deliveriesActions.localStorageToDelivery2(JSON.parse(myStorage.delivery2)));
-    }
-    if (myStorage.delivery3 && myStorage.delivery3 != "{}") {
-      dispatch(deliveriesActions.localStorageToDelivery3(JSON.parse(myStorage.delivery3)));
-    }
-  }, []);
+    // setting the timer
+    const {time, start, pause} = useTimer({
+        autostart: false,
+        initialTime: initialTime,
+        interval: timeInterval,
+    });
 
-  return waitingCustomers.length > 0 ? (
-    <div className={"container"}>
-      <div className={"buttons_container"}>
-        <button onClick={start}>Start</button>
-        <button onClick={pause}>Pause</button>
-        <button onClick={() => {window.localStorage.clear() ; window.location.reload()}}>Reset</button>
-      </div>
-      <div className={"container__left"}>
-        <Customers time={time} />
-        <Delivery time={time} />
-      </div>
-      <Production time={time} />
-    </div>
-  ) : (
-    <div className={"container"}>
-      <button onClick={() => {window.localStorage.clear() ; window.location.reload()}}>Reset</button>
-      <img
-        src={
-          "https://cdn.vox-cdn.com/thumbor/c9bipEBuVa1OnSyZN8HI_Mp2910=/0x0:1500x996/920x613/filters:focal(630x378:870x618):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/66850835/ComingAttractions_Close_6.0.jpg"
+    localStorage.setItem(`time`, JSON.stringify(time));
+
+
+    const waitingCustomers = useSelector((state: RootState) => state.customersSlice.customersState);
+    const queue1 = useSelector((state: RootState) => state.queuesSlice.queue1State);
+    const queue2 = useSelector((state: RootState) => state.queuesSlice.queue2State);
+    const queue3 = useSelector((state: RootState) => state.queuesSlice.queue3State);
+    const closeRestaurant = (waitingCustomers.length == 0 && queue1 == 0 && queue2 == 0 && queue3 == 0)
+
+    //checks localstorage for each state, if there is a stored value, update state
+    useEffect(() => {
+        if (myStorage.waitingCustomers) {
+            dispatch(customersActions.localStorageToWating(JSON.parse(myStorage.waitingCustomers)));
         }
-        alt="CLOSED!"
-      />
-    </div>
-  );
+        if (myStorage.queue1 && myStorage.queue1 != "[]") {
+            dispatch(queuesActions.localStorageToQueue1(JSON.parse(myStorage.queue1)));
+        }
+        if (myStorage.queue2 && myStorage.queue2 != "[]") {
+            dispatch(queuesActions.localStorageToQueue2(JSON.parse(myStorage.queue2)));
+        }
+        if (myStorage.queue3 && myStorage.queue3 != "[]") {
+            dispatch(queuesActions.localStorageToQueue3(JSON.parse(myStorage.queue3)));
+        }
+
+        if (myStorage.slot1 && myStorage.slot1 != "{}") {
+            dispatch(slotsActions.localStorageToSlot1(JSON.parse(myStorage.slot1)));
+        }
+        if (myStorage.slot2 && myStorage.slot2 != "{}") {
+            dispatch(slotsActions.localStorageToSlot2(JSON.parse(myStorage.slot2)));
+        }
+        if (myStorage.slot3 && myStorage.slot3 != "{}") {
+            dispatch(slotsActions.localStorageToSlot3(JSON.parse(myStorage.slot3)));
+        }
+        if (myStorage.delivery1 && myStorage.delivery1 != "{}") {
+            dispatch(deliveriesActions.localStorageToDelivery1(JSON.parse(myStorage.delivery1)));
+        }
+        if (myStorage.delivery2 && myStorage.delivery2 != "{}") {
+            dispatch(deliveriesActions.localStorageToDelivery2(JSON.parse(myStorage.delivery2)));
+        }
+        if (myStorage.delivery3 && myStorage.delivery3 != "{}") {
+            dispatch(deliveriesActions.localStorageToDelivery3(JSON.parse(myStorage.delivery3)));
+        }
+    }, []);
+
+    return !closeRestaurant ? (
+        <div className={"container"}>
+            <div className={"buttons_container"}>
+                <button onClick={start}>Start</button>
+                <button onClick={pause}>Pause</button>
+                <button onClick={() => {
+                    window.localStorage.clear();
+                    window.location.reload()
+                }}>Reset
+                </button>
+            </div>
+            <div className={"container__left"}>
+                <Customers time={time}/>
+                <Delivery time={time}/>
+            </div>
+            <Production time={time}/>
+        </div>
+    ) : (
+        <div className={"container"}>
+            <button onClick={() => {
+                window.localStorage.clear();
+                window.location.reload()
+            }}>Reset
+            </button>
+            <img
+                src={
+                    "https://cdn.vox-cdn.com/thumbor/c9bipEBuVa1OnSyZN8HI_Mp2910=/0x0:1500x996/920x613/filters:focal(630x378:870x618):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/66850835/ComingAttractions_Close_6.0.jpg"
+                }
+                alt="CLOSED!"
+            />
+        </div>
+    );
 };
 
 export default Restaurant;
