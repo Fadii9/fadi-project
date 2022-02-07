d8440c8e8daef7118158d78a5d7db35983dfab68
Fadi <fadi_makhoul1@hotmail.com>
2022-02-07 11:26:26 +0200

    added actions types

--- src/store/reducers/slotsReducer.ts
+++ src/store/reducers/slotsReducer.ts
@@ -1,22 +1,30 @@
-import {CaseReducer, PayloadAction } from "@reduxjs/toolkit";
+import { CaseReducer, PayloadAction } from "@reduxjs/toolkit";
 import { Customer, DeliveryState, SlotState } from "../index";
 
-export const addToSlot: CaseReducer<Record<number, SlotState>, PayloadAction<{ slot: number; customer: SlotState }>> = (
-  state: Record<number, SlotState>,
-  action: PayloadAction<{ slot: number; customer: SlotState }>
+type SlotsSlice = Record<number, SlotState>;
+type AddtoSlot = CaseReducer<
+  Record<number, SlotState>,
+  PayloadAction<{ slot: number; customer: SlotState }>
+>
+type AddtoSlotAction = PayloadAction<{ slot: number; customer: SlotState }>
+type EmptySlotAction = PayloadAction<{ slot: number }>
+
+export const addToSlot: AddtoSlot = (
+  state: SlotsSlice,
+  action: AddtoSlotAction
 ) => {
   const stateName = action.payload.slot;
-  const returnedState: Record<number, SlotState> = { ...state };
+  const returnedState: SlotsSlice = { ...state };
   returnedState[stateName] = action.payload.customer;
   return returnedState;
 };
 
 export const emptySlot = (
-  state: Record<number, DeliveryState>,
-  action: PayloadAction<{ slot: number }>
+  state: SlotsSlice,
+  action: EmptySlotAction
 ) => {
   const stateName = action.payload.slot;
-  const returnedState: Record<number, DeliveryState> = { ...state };
+  const returnedState: SlotsSlice = { ...state };
   returnedState[stateName] = {};
   return returnedState;
 };
--- src/store/utils/functions.ts
+++ src/store/utils/functions.ts
@@ -3,19 +3,19 @@ import { queuesNumber } from "../../data/stationsNumber";
 
 export const createInitialQueuesState = (queuesNumber: number) => {
     const initState: Record<number, Customer[]> = {};
-    for (let i = 1; i <= queuesNumber; i++) {initState[i] = [];}
+    for (let i = 1; i <= queuesNumber; i++) initState[i] = [];
     return initState
 }
 
 export const createInitialSlotsState = (statesNumber: number) => {
     const initState: Record<number, SlotState> = {};
-    for (let i = 1; i <= statesNumber; i++) {initState[i] = {};}
+    for (let i = 1; i <= statesNumber; i++) initState[i] = {}
     return initState
 }
 
 export const createInitialDeliverisState = (deliveriesNumber: number) => {
     let initState: Record<number, DeliveryState> = {};
-    for (let i = 1; i <= deliveriesNumber; i++) {initState[i] = {};}
+    for (let i = 1; i <= deliveriesNumber; i++) initState[i] = {}
     return initState
 }
 
@@ -25,7 +25,7 @@ export const cancelQueueFunction = (state: Record<number, Customer[]>, queueNumb
     delete returnedState[queueNumber]
     const initState: Record<number, Customer[]> = {};
     let temporaryQueues: Customer[][] = []
-    for (let i = 0; i < queuesNumber - 1; i++) {temporaryQueues[i] = []}
+    for (let i = 0; i < queuesNumber - 1; i++) temporaryQueues[i] = []
     for (let i = 0 ; i < customersInCanceledQueue.length ; i++){
         const k = i < temporaryQueues.length? i : i - temporaryQueues.length
         temporaryQueues[k].push(customersInCanceledQueue[i])
